c400eae5e2fa00be38c7b0a1abf865e22e364761
Akash Sharma <shara98@mcmaster.ca>
2021-10-21 23:49:40 -0400

    finished logic

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import Axios from 'axios';
 import './App.css';
 
+
+
 function App() {
+  const [q, setq] = useState('');
+  const [data, setData] = useState('');
+  const day = { Temp: "", Description: "" };
+
+  const getWeather = async () => {
+    try {
+      const res = await Axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${q}&appid=436752e99bf23ca9371cac6b0b58dd9c`);
+      console.log(res);
+      day.Temp = res.data.main.temp;
+      day.Description = res.data.weather[0].description;
+      setData(day);
+    } catch (e) {
+      console.log(e);
+    }
+
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <input id="s" name="searchBar" className="search" onChange={event => setq(event.target.value)}></input>
+      <h1>{q}</h1>
+      <h2>{data.Temp}</h2>
+      <h2>{data.Description}</h2>
+      <button onClick={getWeather}></button>
+    </div >
   );
 }
 
